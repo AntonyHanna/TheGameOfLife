9249b5f9b43da909b5b0e3d6dc74f318531f6add
Antony Hanna <antonyhanna01@gmail.com>
2021-01-25 20:28:36 +1100

    organised header file

--- board.h
+++ board.h
@@ -5,15 +5,26 @@
 #include <unistd.h>
 #include <string.h>
 
-void print_board(char *board, size_t len_x, size_t len_y);
-void populate_live_cells(char *board, size_t len_x, size_t len_y);
-void update_board_state(char *board, size_t len_x, size_t len_y);
-
-uint8_t validate_point(size_t len_x, size_t len_y, int8_t x, int8_t y);
-char get_char_at_point(char *board, size_t len_x, size_t len_y, int8_t x, int8_t y);
-uint8_t count_live_neigbours(char *board, size_t len_x, size_t len_y, int8_t x, int8_t y);
-void begin_simulation(char *board, size_t len_x, size_t len_y);
+/* 
+MUTATION FUNCTIONS 
+Will modify the board passed to it
+*/
 uint8_t set_cell_to_char(char *board, size_t len_x, size_t len_y, int8_t x, int8_t y, char fill);
+void update_board_state(char *board, size_t len_x, size_t len_y);
+void begin_simulation(char *board, size_t len_x, size_t len_y);
+void populate_live_cells(char *board, size_t len_x, size_t len_y);
 
-// turns an 2 dimensional index into 1 dimensional index
-uint16_t get_index(size_t len_y, int8_t x, int8_t y);
+/* 
+GETTERS
+Help in grabbing data from the board
+*/
+char get_char_at_point(char *board, size_t len_x, size_t len_y, int8_t x, int8_t y);
+uint16_t get_index(size_t len_y, int8_t x, int8_t y); /* takes a 2d index and returns a 1d index */
+
+/*
+HELPERS
+These are just useful functions that i don't know how to categorize :)
+*/
+uint8_t validate_point(size_t len_x, size_t len_y, int8_t x, int8_t y);
+uint8_t count_live_neigbours(char *board, size_t len_x, size_t len_y, int8_t x, int8_t y);
+void print_board(char *board, size_t len_x, size_t len_y);
